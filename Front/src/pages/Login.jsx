4ede031cfc4e54ac0a25c22dc586758a3cfeb8f9
Luiz-Miguel-Pimenta <Miguel876528@gmail.com>
2026-06-14 12:34:22 -0300

    Finalizei a as telas de Login e Cadastro com autentificação

--- Front/src/pages/Login.jsx
+++ Front/src/pages/Login.jsx
@@ -1,31 +1,86 @@
 import { useActionState } from "react";
+import { z, ZodError } from "zod";
+import { AxiosError } from "axios";
+import { useNavigate } from "react-router";
+
+import { api } from "../services/api";
+import { useAuth } from "../hooks/useAuth"
 
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
 
+const loginSchema = z.object({
+    email: z.string().email({ message: "E-mail inválido" }),
+    senha: z.string().min(6, { message: "Informe a senha" }),
+});
+
 export function Login() {
-    const [email, setEmail] = useState("");
-    const [senha, setSenha] = useState("");
+    const [state, formAction, isLoading] = useActionState(fazerLogin, null);//null = estado inicial
 
-    async function onSubmit() {
+    const navigate = useNavigate();
+    const auth = useAuth();
 
+    async function fazerLogin(_, formData) {
+        try {
+            const data = loginSchema.parse({
+                email: formData.get("email"),
+                senha: formData.get("senha")
+            })
+
+            const response = await api.post("/usuario/login", data);
+            auth.login(response.data)
+
+        } catch (error) {
+            console.log(error);
+
+            if(error instanceof ZodError) {
+                return { message: error.issues[0].message };
+            };
+
+            if(error instanceof AxiosError) {
+                return { message: error.response?.data.message || "Erro no servidor" };
+            }
+
+            return { message: "Não foi possível entrar"};
+        }
     }
 
     return (
-        <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
-            <Input 
-                legend="E-mail"
-                type="email"
-                placeholder="seu@email.com"
-                required
-            />
-        
-            <Input 
-                legend="Senha"
-                type="password"
-                placeholder="******"
-                required
-            />
-        </form>
+        <div className="w-full flex flex-col">
+            <form action={formAction} className="w-full flex flex-col gap-4">
+                <Input 
+                    legend="E-mail"
+                    type="email"
+                    required
+                    name="email"
+                    placeholder="seu@email.com"
+                />
+            
+                <Input 
+                    legend="Senha"
+                    type="password"
+                    required
+                    name="senha"
+                    placeholder="******"
+                />
+                {state?.message && !isLoading && (
+                    <p className="text-sm text-red-600 text-center my-2 font-medium">
+                        {state.message}
+                    </p>
+                )}
+
+                <Button type="submit" isLoading={isLoading}>
+                    Entrar
+                </Button>
+            </form>
+
+            <button
+                onClick={() => navigate("/cadastrar")}
+                className="text-sm font-semibold text-slate-500 
+                mt-6 text-center hover:text-blue-600 cursor-pointer"
+            >
+                Criar uma conta
+            </button>
+        </div>
     )
 }
